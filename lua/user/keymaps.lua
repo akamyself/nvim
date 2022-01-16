961bbae4ebe75adf9212bd507c9ca9fe92f70d0a
nckmcfls <nckmcfls@protonmail.com>
2022-01-16 10:30:06 +0100

    keymaps: fix typo

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -31,7 +31,7 @@ keymap("n", "<C-Down>", ":resize +2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
--- Naviagate buffers
+-- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
